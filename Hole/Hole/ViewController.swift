dc104c407ee4aadf995e16723233e3a5ecfef752
tanhaipeng <tanhp@outlook.com>
2017-09-09 14:56:55 +0800

    Hole app init

---
+++ Hole/Hole/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hole
+//
+//  Created by tanhaipeng on 2017/9/9.
+//  Copyright © 2017年 tanhaipeng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
